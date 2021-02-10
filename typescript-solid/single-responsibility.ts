e1b5d1b00bfe5a9c8a9f52d56fb0a3eb78b781f7
jaimegsandoval <jaime.devs@gmail.com>
2021-02-09 19:34:53 -0800

    Added srp

---
+++ typescript-solid/single-responsibility.ts
@@ -0,0 +1,66 @@
+let httpClient = new HttpClient();
+filterEmployees(httpClient, []).then(employees => {
+      displayEmployees(employees);
+      recordEmployee(employees, httpClient);
+});
+
+export function filterEmployees(httpClient, filters): Promise<any> {
+  try {
+    filters = removeDuplicateFilters(filters);
+    return httpClient.get("/api/employees").then(employees => {
+
+      let result = applyFilters(employees);
+      return result;
+
+    }).catch(err => {
+      handleError(err, "Something went wrong while processing the employee");
+    });
+  }
+
+  catch(err) {
+     handleError(err, "Application critical error");
+  }
+}
+
+
+function recordEmployee(result: any[], httpClient: any) {
+  result.forEach(employee => {
+    httpClient.post("/api/employees/record", employee);
+  });
+}
+
+function displayEmployees(result: any[]) {
+  let list = document.getElementById("employees-list");
+  result.forEach(employee => {
+    let employeeItem = document.createElement('li');
+    employeeItem.innerHTML = `<strong>${employee.lastTime}</strong> ${employee.firstName}`;
+    list.appendChild(employeeItem);
+  });
+}
+
+function handleError(err, msg) {
+  console.error(msg);
+  let messageBox = new MessageBox();
+  messageBox.show(msg);
+}
+
+
+
+
+
+// code below here is to stop linting errors. ignore them
+function removeDuplicateFilters(filters) {
+  // exists only to stop lint errors
+}
+
+function applyFilters(filters): any[]{
+  // exists only to stop lint errors
+  return [];
+}
+
+export class MessageBox {
+  // exists only to stop lint errors
+  show(msg) {
+
+  }
+}
